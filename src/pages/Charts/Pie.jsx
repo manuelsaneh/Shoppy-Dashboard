cde7d27fa28c47d366fbc2f717f67f6d0989008e
Manuel Saneh <man.saneh10@gmail.com>
2023-01-08 19:09:42 +0200

    first commit

---
+++ src/pages/Charts/Pie.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { pieChartData } from '../../data/dummy'
+import { Header, Pie as PieChart } from '../../components'
+
+function Pie() {
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Pie" title="Project Cost Breakdown" />
+      <div className="w-full">
+        <PieChart id="chart-pie" data={pieChartData} legendVisiblity height="full" />
+      </div>
+    </div>
+  )
+}
+
+export default Pie
